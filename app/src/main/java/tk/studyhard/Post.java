91589a0bad603c58d349e0184a362f9997178e4d
Chelayos <chelayos@hotmail.com>
2016-09-16 18:53:48 +0200

    Add categories

--- app/src/main/java/tk/studyhard/Post.java
+++ app/src/main/java/tk/studyhard/Post.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.widget.Toast;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
